faca55f08c38b1ec1fcf7345499282fca99eb224
Cristian Holguin <cristian.holguin@ibm.com>
2021-09-08 12:05:59 -0500

    Update Script

--- cli-script/delete-script.sh
+++ cli-script/delete-script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+primary="jp-osa"
+secondary="jp-tok"
+
+ibmcloud target -r $primary
 while read i
     do ibmcloud is instance-delete $i -f
 done < vsis-primary-ids.txt
 
+
+ibmcloud target -r $secondary
 while read i
     do ibmcloud is instance-delete $i -f
 done < vsis-secondary-ids.txt
