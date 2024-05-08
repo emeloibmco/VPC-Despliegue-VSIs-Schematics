d599d86793889da90447aea205ec296fd2e05495
diegorom27 <diegorom27@gmail.com>
2024-05-08 18:03:28 -0500

    Update vars.tf
    delete duplicated var

--- vars.tf
+++ vars.tf
@@ -25,6 +25,9 @@ variable "image_vsi_pr" {
 variable "name_vpc_sec" {
   description = "vpc name secondary region"
 }
+variable "image_vsi_sec" {
+  description = "vsi image secondary region"
+}
 variable "name_subnet_pr" {
   description = "subnet name primary region"
 }
@@ -35,18 +38,12 @@ variable "subnet_zone_pr" {
 variable "name_subnet_sec" {
   description = "subnet name secondary region"
 }
-variable "image_vsi_pr" {
-  description = "vsi image primary region"
-}
 variable "subnet_zone_sec" {
   default = "1"
   description = "number that identify the zone"
 }
 variable "count-vsi" {
   description = "number of vsi"
-}
-variable "image_vsi_sec" {
-  description = "vsi image secondary region"
 }
 variable "resource_group" {
   description = "resource group name"
