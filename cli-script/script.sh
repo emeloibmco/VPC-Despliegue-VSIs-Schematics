093513eb163ec483aeb5dc1a830a2994d42fe1c9
diegorom27 <diegorom27@gmail.com>
2025-07-22 18:14:55 -0500

    Update script.sh

--- cli-script/script.sh
+++ cli-script/script.sh
@@ -4,58 +4,88 @@
 # Variables de despliegue
 #####################################################################################################
 
-#Regiones de despliegue
-primary="jp-osa"
-secondary="jp-tok"
-BaseName="cce-vsi-provisioning"
+# Regiones de despliegue
+primary="us-south"
+secondary="us-east"
 
 # Primary
-vpcosa="vpc-demo-osa"
-subnetosa="subnet-demo-osa"
-keyosa="key-demo-osa"
+vpc_primary="vpc-dall"
+subnet_primary="sn-20250721-01"
+key_primary="key-dall"
 
 # Secondary
-vpctok="vpc-demo-tok"
-subnettok="subnet-demo-tok"
-keytok="key-demo-tok"
+vpc_secondary="vpc-wash"
+subnet_secondary="sn-20250722-01"
+key_secondary="key-dall"
 
-resourcegroup="vpc-demo-rg"
+BaseName="vsi"
+resourcegroup="cce-valtadria"
+profile="cx2-4x8"
 
 #####################################################################################################
 # No editar
 #####################################################################################################
 
-idsprimary=""
-idssecondary=""
-profile="cx2-4x8"
+# Obtener imagen pública de CentOS
 ibmcloud target -r $primary
-imageosaid=$(ibmcloud is images | grep -i "ibm-centos-8.*available.*amd64.*public" | cut -d" " -f1)
-vpcosaid=$(ibmcloud is vpcs | grep -i $vpcosa | cut -d" " -f1)
-keyosaid=$(ibmcloud is keys | grep -i $keyosa | cut -d" " -f1)
-subnetosaid=$(ibmcloud is subnets | grep -i $subnetosa | cut -d" " -f1)
+imageid=$(ibmcloud is images --visibility public | grep -Ei "ibm-centos.*stream.*9.*available.*amd64" | head -n1 | awk '{print $1}')
 
-for i in {1..50}
-do
-   var1osa=$(ibmcloud is instance-create $BaseName-primary-$i $vpcosaid $primary-1 $profile $subnetosaid --image-id $imageosaid --key-ids $keyosaid --resource-group-name $resourcegroup)
-   echo "Instancia $BaseName-primary-$i Creada"
-   var2osa=${var1osa/Name */}
-   var2osa=${var2osa/ /}
-   idsprimary="${var2osa/*ID /}\n$idsprimary"
-done
-echo -e $idsprimary > vsis-primary-ids.txt
+# Obtener recursos región primaria
+vpcid_primary=$(ibmcloud is vpcs | grep -i "$vpc_primary" | awk '{print $1}')
+keyid_primary=$(ibmcloud is keys | grep -i "$key_primary" | awk '{print $1}')
+subnetid_primary=$(ibmcloud is subnets | grep -i "$subnet_primary" | awk '{print $1}')
 
+# Obtener recursos región secundaria
 ibmcloud target -r $secondary
-imagetokid=$(ibmcloud is images | grep -i "ibm-centos-8.*available.*amd64.*public" | cut -d" " -f1)
-vpctokid=$(ibmcloud is vpcs | grep -i $vpctok | cut -d" " -f1)
-keytokid=$(ibmcloud is keys | grep -i $keytok | cut -d" " -f1)
-subnettokid=$(ibmcloud is subnets | grep -i $subnettok | cut -d" " -f1)
+imageid_sec=$(ibmcloud is images --visibility public | grep -Ei "ibm-centos.*stream.*9.*available.*amd64" | head -n1 | awk '{print $1}')
+vpcid_secondary=$(ibmcloud is vpcs | grep -i "$vpc_secondary" | awk '{print $1}')
+keyid_secondary=$(ibmcloud is keys | grep -i "$key_secondary" | awk '{print $1}')
+subnetid_secondary=$(ibmcloud is subnets | grep -i "$subnet_secondary" | awk '{print $1}')
 
-for i in {1..50}
-do
-   var1tok=$(ibmcloud is instance-create $BaseName-secondary-$i $vpctokid $secondary-1 $profile $subnettokid --image-id $imagetokid --key-ids $keytokid --resource-group-name $resourcegroup)
-   echo "Instancia $BaseName-secondary-$i Creada"
-   var2tok=${var1tok/Name */}
-   var2tok=${var2tok/ /}
-   idssecondary="${var2tok/*ID /}\n$idssecondary"
+# Crear instancias en primaria
+ibmcloud target -r $primary -g "$resourcegroup"
+> vsis-primary-ids.txt
+
+for i in {1..50}; do
+  (
+    name="$BaseName-$primary-$i"
+    output=$(ibmcloud is instance-create "$name" "$vpcid_primary" "$primary-1" "$profile" "$subnetid_primary" --image "$imageid" --keys "$keyid_primary" --resource-group-name "$resourcegroup")
+    echo "Instancia $name Creada"
+    id=$(echo "$output" | grep -oP '(?<=ID\s)[a-z0-9\-]+')
+    echo "$id" > "tmp-id-primary-$i.txt"
+  ) &
 done
-echo -e $idssecondary > vsis-secondary-ids.txt
+
+wait
+
+for i in {50..1}; do
+  if [ -f "tmp-id-primary-$i.txt" ]; then
+    cat "tmp-id-primary-$i.txt"
+  fi
+done > vsis-primary-ids.txt
+
+rm tmp-id-primary-*.txt
+
+# Crear instancias en secundaria
+ibmcloud target -r $secondary -g "$resourcegroup"
+> vsis-secondary-ids.txt
+
+for i in {1..50}; do
+  (
+    name="$BaseName-$secondary-$i"
+    output=$(ibmcloud is instance-create "$name" "$vpcid_secondary" "$secondary-1" "$profile" "$subnetid_secondary" --image "$imageid_sec" --keys "$keyid_secondary" --resource-group-name "$resourcegroup")
+    echo "Instancia $name Creada"
+    id=$(echo "$output" | grep -oP '(?<=ID\s)[a-z0-9\-]+')
+    echo "$id" > "tmp-id-secondary-$i.txt"
+  ) &
+done
+
+wait
+
+for i in {50..1}; do
+  if [ -f "tmp-id-secondary-$i.txt" ]; then
+    cat "tmp-id-secondary-$i.txt"
+  fi
+done > vsis-secondary-ids.txt
+
+rm tmp-id-secondary-*.txt
